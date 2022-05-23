063d62ff48457ab3a4d9366557383c78be7affa0
FPa-riken <frederic.papazian@riken.jp>
2022-05-24 08:39:37 +0900

    fix: handle macOS specifics of Docker networking

--- manager/manager_start.sh
+++ manager/manager_start.sh
@@ -5,8 +5,9 @@ cd ${DIRNAME}
 # set environment variable needed by manager
 set -o allexport; source abart-manager.env ; set +o allexport;
 
-# create a private virtual network to link manager to worker(s) (no access to external network)
-sudo docker network create --driver bridge --internal ${ABART_PRIVATE_NET}
+# create a virtual network to link manager to worker(s) (no access to external network)
+sudo docker network rm  ${ABART_PRIVATE_NET}
+sudo docker network create --driver bridge ${ABART_PRIVATE_NET}
 
 # start manager (in detached mode) connected to the private virtual network exposing its rest API
 printf ' abart-manager\t'
